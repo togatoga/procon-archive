d9b98bf3a89efd6fffc6a57e0979e8af75f0ed1c
togatoga <togasakitogatoga@gmail.com>
2017-09-02 21:02:47 +0900

    [AC] arc082 arc082_a

---
+++ atcoder.jp/arc082/arc082_a/Main.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+// c++11
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+#define mp make_pair
+#define mt make_tuple
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+
+const int INF = 1 << 29;
+const double EPS = 1e-9;
+const ll MOD = 1000000007;
+
+const int dx[] = {1, 0, -1, 0}, dy[] = {0, -1, 0, 1};
+int N;
+vector<int> A;
+map<int, int> cnt;
+int main() {
+  cin >> N;
+  A.resize(N);
+  for (auto &val : A){
+    cin >> val;
+  }
+  for (int i = 0; i < N; i++){
+    cnt[A[i]]++;
+    cnt[A[i] - 1]++;
+    cnt[A[i] + 1]++;
+  }
+  int res = 0;
+  for (const auto &val : cnt){
+    res = max(res, val.second);
+  }
+  cout << res << endl;
+  return 0;
+}
