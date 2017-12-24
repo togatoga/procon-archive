c3ea1596cd53ea1212f0249bb67ef9e2af0e3d7c
togatoga <togasakitogatoga@gmail.com>
2017-12-24 10:49:52 +0900

    [AC] bcu30 bcu30_b

---
+++ atcoder.jp/bcu30/bcu30_b/Main.go
@@ -0,0 +1,240 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	maxHW = 9
+)
+
+func isMoveValid(board [maxHW]string, y, x int) bool {
+	dx := []int{2, 1, -1, -2, -2, -1, 1, 2}
+	dy := []int{-1, -2, -2, -1, 1, 2, 2, 1}
+	v := board[y][x]
+	for i := 0; i < len(dx); i++ {
+		my := y + dy[i]
+		mx := x + dx[i]
+		if my < 0 || my >= maxHW || mx < 0 || mx >= maxHW {
+			continue
+		}
+		if board[my][mx] == v {
+			return false
+		}
+	}
+	return true
+}
+func isVerticalValid(board [maxHW]string, y int) bool {
+	values := map[byte]int{}
+	for i := 0; i < maxHW; i++ {
+		values[board[y][i]]++
+		if values[board[y][i]] > 1 {
+			return false
+		}
+	}
+	return true
+}
+func isHorizontalValid(board [maxHW]string, x int) bool {
+	values := map[byte]int{}
+	for i := 0; i < maxHW; i++ {
+		values[board[i][x]]++
+		if values[board[i][x]] > 1 {
+			return false
+		}
+	}
+	return true
+}
+func main() {
+	var board [maxHW]string
+	fsc := NewFastScanner()
+	for i := 0; i < maxHW; i++ {
+		board[i] = fsc.Next()
+	}
+	for i := 0; i < maxHW; i++ {
+		if !isHorizontalValid(board, i) {
+			fmt.Println("No")
+			return
+		}
+	}
+	for i := 0; i < maxHW; i++ {
+		if !isVerticalValid(board, i) {
+			fmt.Println("No")
+			return
+		}
+	}
+	for i := 0; i < maxHW; i++ {
+		for j := 0; j < maxHW; j++ {
+			if !isMoveValid(board, i, j) {
+				fmt.Println("No")
+				return
+			}
+		}
+	}
+
+	fmt.Println("Yes")
+}
+
+//template
+type FastScanner struct {
+	r   *bufio.Reader
+	buf []byte
+	p   int
+}
+
+func NewFastScanner() *FastScanner {
+	rdr := bufio.NewReaderSize(os.Stdin, 1024)
+	return &FastScanner{r: rdr}
+}
+func (s *FastScanner) Next() string {
+	s.pre()
+	start := s.p
+	for ; s.p < len(s.buf); s.p++ {
+		if s.buf[s.p] == ' ' {
+			break
+		}
+	}
+	result := string(s.buf[start:s.p])
+	s.p++
+	return result
+}
+func (s *FastScanner) NextLine() string {
+	s.pre()
+	start := s.p
+	s.p = len(s.buf)
+	return string(s.buf[start:])
+}
+func (s *FastScanner) NextInt() int {
+	v, _ := strconv.Atoi(s.Next())
+	return v
+}
+func (s *FastScanner) NextInt64() int64 {
+	v, _ := strconv.ParseInt(s.Next(), 10, 64)
+	return v
+}
+
+func (s *FastScanner) NextIntArray() []int {
+	s.pre()
+	start := s.p
+	result := []int{}
+	for ; s.p < len(s.buf)+1; s.p++ {
+		if s.p == len(s.buf) || s.buf[s.p] == ' ' {
+			v, _ := strconv.ParseInt(string(s.buf[start:s.p]), 10, 0)
+			result = append(result, int(v))
+			start = s.p + 1
+		}
+	}
+
+	return result
+}
+
+func (s *FastScanner) NextInt64Array() []int64 {
+	s.pre()
+	start := s.p
+	result := []int64{}
+	for ; s.p < len(s.buf)+1; s.p++ {
+		if s.p == len(s.buf) || s.buf[s.p] == ' ' {
+			v, _ := strconv.ParseInt(string(s.buf[start:s.p]), 10, 64)
+			result = append(result, v)
+			start = s.p + 1
+		}
+	}
+	return result
+}
+
+func (s *FastScanner) pre() {
+	if s.p >= len(s.buf) {
+		s.readLine()
+		s.p = 0
+	}
+}
+func (s *FastScanner) readLine() {
+	s.buf = make([]byte, 0)
+	for {
+		l, p, e := s.r.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+		s.buf = append(s.buf, l...)
+		if !p {
+			break
+		}
+	}
+}
+
+//Max,Min
+func IntMax(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func Int64Max(a, b int64) int64 {
+	if a < b {
+		return b
+	}
+	return a
+}
+func Float64Max(a, b float64) float64 {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func IntMin(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func Int64Min(a, b int64) int64 {
+	if a > b {
+		return b
+	}
+	return a
+}
+func Float64Min(a, b float64) float64 {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+//Gcd
+func IntGcd(a, b int) int {
+	if a < b {
+		b, a = a, b
+	}
+	if b == 0 {
+		return a
+	}
+	return IntGcd(b, a%b)
+}
+func Int64Gcd(a, b int64) int64 {
+	if a < b {
+		b, a = a, b
+	}
+	if b == 0 {
+		return a
+	}
+	return Int64Gcd(b, a%b)
+}
+
+func IntAbs(a int) int {
+	if a < 0 {
+		a *= -1
+	}
+	return a
+}
+
+func Int64Abs(a int64) int64 {
+	if a < 0 {
+		a *= -1
+	}
+	return a
+}
