9425d73e4c943ca6e08c5081e64465f0d57815b4
togatoga <togasakitogatoga@gmail.com>
2018-03-03 07:54:48 +0900

    [AC] abc083 abc083_b

---
+++ atcoder.jp/abc083/abc083_b/Main.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+// c++11
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+#define mp make_pair
+#define mt make_tuple
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+
+const int INF = 1 << 29;
+const ll LL_INF = 1LL << 60;
+const double EPS = 1e-9;
+const ll MOD = 1000000007;
+
+const int dx[] = {1, 0, -1, 0}, dy[] = {0, -1, 0, 1};
+int N, A, B;
+int sum;
+int main()
+{
+    cin >> N >> A >> B;
+    for (int i = 1; i <= N; i++)
+    {
+        int val = i;
+        int result = 0;
+        while (val > 0)
+        {
+            result += val % 10;
+            val /= 10;
+        }
+        if (result >= A and result <= B)
+        {
+            sum += i;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
