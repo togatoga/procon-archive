764dc947f86794e43166d16e97edbb7db2189789
togatoga <togasakitogatoga@gmail.com>
2018-04-16 18:04:30 +0900

    [AC] abc092 abc092_b

---
+++ atcoder.jp/abc092/abc092_b/Main.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+// c++11
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+#define mp make_pair
+#define mt make_tuple
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+
+const int INF = 1 << 29;
+const ll LL_INF = 1LL << 60;
+const double EPS = 1e-9;
+const ll MOD = 1000000007;
+
+const int dx[] = {1, 0, -1, 0}, dy[] = {0, -1, 0, 1};
+int N,D,X;
+vector<int> A;
+int main() {
+  cin >> N;
+  cin >> D >> X;
+  A.resize(N);
+  for (auto &val : A){
+    cin >> val;
+  }
+  sort(A.begin(), A.end());
+  int sum = 0;
+  for (int j = 0; j < A.size(); j++){
+    int day = 1;
+    while (day <= D){
+      sum++;
+      day += A[j];
+    }
+  }
+  cout << sum + X << endl;
+  return 0;
+}
