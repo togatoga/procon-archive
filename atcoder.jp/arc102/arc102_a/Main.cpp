419a0756f394ae3fa3ad5e836223337b282535f3
togatoga <togasakitogatoga@gmail.com>
2018-09-03 18:57:42 +0900

    [AC] arc102 arc102_a

---
+++ atcoder.jp/arc102/arc102_a/Main.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+// c++11
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+#define mp make_pair
+#define mt make_tuple
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+template <class T>
+using max_priority_queue = priority_queue<T>;
+template <class T>
+using min_priority_queue = priority_queue<T, std::vector<T>, std::greater<T>>;
+
+const int INF = 1 << 29;
+const ll LL_INF = 1LL << 60;
+const double EPS = 1e-9;
+const ll MOD = 1000000007;
+
+const int dx[] = {1, 0, -1, 0}, dy[] = {0, -1, 0, 1};
+int N, K;
+int main()
+{
+  cin >> N >> K;
+  ll res = 0;
+  if (K % 2 != 0)
+  {
+    ll a = N / K;
+    res = a * a * a;
+    cout << res << endl;
+    return 0;
+  }
+  else
+  {
+    ll a = N / K;
+    ll b = K / 2;
+    res = a * a * a;
+    if (N >= b)
+    {
+      ll c = (N - b) / K + 1;
+      res += c * c * c;
+    }
+
+    cout << res << endl;
+    return 0;
+  }
+
+  return 0;
+}
