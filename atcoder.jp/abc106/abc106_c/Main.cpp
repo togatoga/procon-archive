98451dc98b60da6ef7bcd4f5cd40b86183a4bfd4
togatoga <togasakitogatoga@gmail.com>
2018-09-03 19:54:57 +0900

    [AC] abc106 abc106_c

---
+++ atcoder.jp/abc106/abc106_c/Main.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+// c++11
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+#define mp make_pair
+#define mt make_tuple
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+template <class T>
+using max_priority_queue = priority_queue<T>;
+template <class T>
+using min_priority_queue = priority_queue<T, std::vector<T>, std::greater<T>>;
+
+const int INF = 1 << 29;
+const ll LL_INF = 1LL << 60;
+const double EPS = 1e-9;
+const ll MOD = 1000000007;
+
+const int dx[] = {1, 0, -1, 0}, dy[] = {0, -1, 0, 1};
+string S;
+ll K;
+int N;
+int main() {
+  cin >> S;
+  cin >> K;
+  N = S.size();
+  ll src = -1;
+  for (int i = 0; i < N; i++)
+  {
+    if (S[i] != '1')
+    {
+      src = i;
+      break;
+    }
+  }
+  if (src <= -1 || K <= src)
+  {
+    cout << 1 << endl;
+    return 0;
+  }
+  cout << S[src] << endl;
+  return 0;
+}
