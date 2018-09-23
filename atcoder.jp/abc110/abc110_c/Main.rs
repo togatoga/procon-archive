3355431b3aae19c3192a218e729c380b35b5b5a9
togatoga <togasakitogatoga@gmail.com>
2018-09-24 05:26:06 +0900

    [AC] abc110 abc110_c

---
+++ atcoder.jp/abc110/abc110_c/Main.rs
@@ -0,0 +1,79 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::process;
+
+fn main() {
+    input! {
+        S: chars,
+        T: chars,
+    }
+    let mut map1: HashMap<char, char> = HashMap::new();
+    let mut map2: HashMap<char, char> = HashMap::new();
+    for (&c1, &c2) in S.iter().zip(T.iter()) {
+        if let Some(c) = map1.insert(c1, c2) {
+            if c != c2 {
+                println!("No");
+                process::exit(0);
+            }
+        }
+        if let Some(c) = map2.insert(c2, c1) {
+            if c != c1 {
+                println!("No");
+                process::exit(0);
+            }
+        }
+    }
+    println!("Yes");
+}
