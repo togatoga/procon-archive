50c049e31fd9862ac2c16096474a3760381a59e9
togatoga <togasakitogatoga@gmail.com>
2018-10-25 14:50:23 +0900

    [AC] soundhound2018-summer-qual soundhound2018_summer_qual_a

---
+++ atcoder.jp/soundhound2018-summer-qual/soundhound2018_summer_qual_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    if (a + b == 15)
+    {
+        cout << "+" << endl;
+    }else if( a * b == 15)
+    {
+        cout << "*" << endl;
+    }else{
+        cout << "x" << endl;
+    }
+
+}
