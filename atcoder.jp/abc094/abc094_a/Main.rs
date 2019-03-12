8c6c5ff1f0422b2e40ce619e81f137022bd2ae0e
togatoga <togasakitogatoga@gmail.com>
2019-03-13 08:23:09 +0900

    [AC] abc094 abc094_a

---
+++ atcoder.jp/abc094/abc094_a/Main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let a: usize = sc.read();
+    let b: usize = sc.read();
+    let x = sc.read();
+    for i in 0..b + 1 {
+        let sum = a + i;
+        if sum == x {
+            println!("YES");
+            return;
+        }
+    }
+    println!("NO");
+}
+
+//snippet from kenkoooo
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
