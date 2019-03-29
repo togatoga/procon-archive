648a576ae6eae73c3761cd59c901d4e707627cac
togatoga <togasakitogatoga@gmail.com>
2019-03-29 14:06:42 +0900

    [AC] abc113 abc113_b

---
+++ atcoder.jp/abc113/abc113_b/Main.rs
@@ -0,0 +1,59 @@
+fn main() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let n: usize = sc.read();
+    let t: usize = sc.read();
+    let a: i32 = sc.read();
+    let res = (0..n).map(|i|
+        {
+            let x = sc.read::<usize>();
+            let tmp: f64 = t as f64 - x as f64 * 0.006;
+            let res = (a as f64 - tmp).abs();
+            res
+        }).enumerate().min_by(|x, y| x.1.partial_cmp(&y.1).unwrap()).unwrap();
+
+    println!("{}", res.0 + 1);
+}
+
+//snippet from kenkoooo
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self.stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_line(&mut self) -> String {
+        use std::io::Read;
+        let buf = self.stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
