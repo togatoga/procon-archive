6379d283b21e102251f8f587c75523337eef2b1a
togatoga <togasakitogatoga@gmail.com>
2019-04-13 21:28:32 +0900

    [AC] abc124 abc124_c

---
+++ atcoder.jp/abc124/abc124_c/Main.rs
@@ -0,0 +1,82 @@
+fn solve() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let s = sc.chars();
+    let res1 = s.iter().enumerate().fold(0, |sum, (idx, &c)| {
+        if idx % 2 == 0 {
+            if c == '1' {
+                return sum + 1;
+            }
+        } else {
+            if c == '0' {
+                return sum + 1;
+            }
+        }
+        sum
+    });
+    let res2 = s.iter().enumerate().fold(0, |sum, (idx, &c)| {
+        if idx % 2 == 0 {
+            if c == '0' {
+                return sum + 1;
+            }
+        } else {
+            if c == '1' {
+                return sum + 1;
+            }
+        }
+        sum
+    });
+    println!("{}", std::cmp::min(res1, res2));
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(64 * 1024 * 1024) // 64MB
+        .spawn(|| solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+//snippet from kenkoooo
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self.stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_line(&mut self) -> String {
+        use std::io::Read;
+        let buf = self.stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
