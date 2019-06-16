10c11c7d568817b8e59c197300f24886fb123530
togatoga <togasakitogatoga@gmail.com>
2019-06-16 21:05:58 +0900

    [AC] abc130 abc130_b

---
+++ atcoder.jp/abc130/abc130_b/Main.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+// c++11
+#include <array>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+#define mp make_pair
+#define mt make_tuple
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+template <class T>
+using max_priority_queue = priority_queue<T>;
+template <class T>
+using min_priority_queue = priority_queue<T, std::vector<T>, std::greater<T>>;
+
+const int INF = 1 << 29;
+const ll LL_INF = 1LL << 60;
+const double EPS = 1e-9;
+const ll MOD = 1000000007;
+
+const int dx[] = {1, 0, -1, 0}, dy[] = {0, -1, 0, 1};
+int N, X;
+vector<int> L;
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin >> N >> X;
+    L.resize(N);
+    for (auto &val : L)
+    {
+        cin >> val;
+    }
+    int d = 0;
+    int cnt = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (d <= X)
+        {
+            cnt++;
+        }
+        d = d + L[i];
+    }
+    if (d <= X)
+    {
+        cnt++;
+    }
+    cout << cnt << endl;
+    return 0;
+}
