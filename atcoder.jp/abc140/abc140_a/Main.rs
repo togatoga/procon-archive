940c85bf8adf462e06e0fadd4f5ce1b3cb16a595
togatoga <togasakitogatoga@gmail.com>
2019-10-08 06:12:07 +0900

    [AC] abc140 abc140_a

---
+++ atcoder.jp/abc140/abc140_a/Main.rs
@@ -0,0 +1,60 @@
+fn solve() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let n: usize = sc.read();
+    println!("{}", n * n * n);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(64 * 1024 * 1024) // 64MB
+        .spawn(|| solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+//snippet from kenkoooo
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_line(&mut self) -> String {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
