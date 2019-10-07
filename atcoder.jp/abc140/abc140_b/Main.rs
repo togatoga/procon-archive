efd0a62533dfabdceb15056383b91b3578a3d78c
togatoga <togasakitogatoga@gmail.com>
2019-10-08 06:24:58 +0900

    [AC] abc140 abc140_b

---
+++ atcoder.jp/abc140/abc140_b/Main.rs
@@ -0,0 +1,73 @@
+fn solve() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let n: usize = sc.read();
+    let a: Vec<usize> = sc.vec(n);
+    let b: Vec<usize> = sc.vec(n);
+    let c: Vec<usize> = sc.vec(n - 1);
+    let mut result = b.iter().fold(0, |sum, b| sum + b);
+    let mut pre = a[0];
+    for i in 1..n {
+        if pre + 1 == a[i] {
+            //let idx = a[i] - pre - 1;
+            result += c[pre - 1];
+            //println!("{} {} {} {}", pre, a[i], i - 1, c[i - 1]);
+        }
+        pre = a[i];
+    }
+    println!("{}", result);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(64 * 1024 * 1024) // 64MB
+        .spawn(|| solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+//snippet from kenkoooo
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_line(&mut self) -> String {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
