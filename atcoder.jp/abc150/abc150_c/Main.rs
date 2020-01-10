1dfc495add4348fb2447a2ec82c1298db887af20
togatoga <togasakitogatoga@gmail.com>
2020-01-10 21:17:10 +0900

    [AC] abc150 abc150_c

---
+++ atcoder.jp/abc150/abc150_c/Main.rs
@@ -0,0 +1,178 @@
+fn solve() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let n: usize = sc.read();
+    let p: Vec<usize> = sc.vec(n);
+    let q: Vec<usize> = sc.vec(n);
+    let mut op = p.clone();
+    op.sort();
+    let mut results = vec![];
+    results.push(op.clone());
+    while op.next_permutation() {
+        results.push(op.clone());
+    }
+    let mut a = 0;
+    let mut b = 0;
+    for i in 0..results.len() {
+        if (results[i] == p) {
+            a = i;
+        }
+        if (results[i] == q) {
+            b = i;
+        }
+    }
+    let result = if a >= b { a - b } else { b - a };
+    println!("{}", result);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(64 * 1024 * 1024) // 64MB
+        .spawn(|| solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+//snippet from kenkoooo
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_line(&mut self) -> String {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
+
+pub trait LexicalPermutation {
+    /// Return `true` if the slice was permuted, `false` if it is already
+    /// at the last ordered permutation.
+    fn next_permutation(&mut self) -> bool;
+    /// Return `true` if the slice was permuted, `false` if it is already
+    /// at the first ordered permutation.
+    fn prev_permutation(&mut self) -> bool;
+}
+
+impl<T> LexicalPermutation for [T]
+where
+    T: Ord,
+{
+    /// Original author in Rust: Thomas Backman <serenity@exscape.org>
+    fn next_permutation(&mut self) -> bool {
+        // These cases only have 1 permutation each, so we can't do anything.
+        if self.len() < 2 {
+            return false;
+        }
+
+        // Step 1: Identify the longest, rightmost weakly decreasing part of the vector
+        let mut i = self.len() - 1;
+        while i > 0 && self[i - 1] >= self[i] {
+            i -= 1;
+        }
+
+        // If that is the entire vector, this is the last-ordered permutation.
+        if i == 0 {
+            return false;
+        }
+
+        // Step 2: Find the rightmost element larger than the pivot (i-1)
+        let mut j = self.len() - 1;
+        while j >= i && self[j] <= self[i - 1] {
+            j -= 1;
+        }
+
+        // Step 3: Swap that element with the pivot
+        self.swap(j, i - 1);
+
+        // Step 4: Reverse the (previously) weakly decreasing part
+        self[i..].reverse();
+
+        true
+    }
+
+    fn prev_permutation(&mut self) -> bool {
+        // These cases only have 1 permutation each, so we can't do anything.
+        if self.len() < 2 {
+            return false;
+        }
+
+        // Step 1: Identify the longest, rightmost weakly increasing part of the vector
+        let mut i = self.len() - 1;
+        while i > 0 && self[i - 1] <= self[i] {
+            i -= 1;
+        }
+
+        // If that is the entire vector, this is the first-ordered permutation.
+        if i == 0 {
+            return false;
+        }
+
+        // Step 2: Reverse the weakly increasing part
+        self[i..].reverse();
+
+        // Step 3: Find the rightmost element equal to or bigger than the pivot (i-1)
+        let mut j = self.len() - 1;
+        while j >= i && self[j - 1] < self[i - 1] {
+            j -= 1;
+        }
+
+        // Step 4: Swap that element with the pivot
+        self.swap(i - 1, j);
+
+        true
+    }
+}
+
+#[test]
+fn lexical() {
+    let mut data = [1, 2, 3];
+    data.next_permutation();
+    assert_eq!(&data, &[1, 3, 2]);
+    data.next_permutation();
+    assert_eq!(&data, &[2, 1, 3]);
+    data.prev_permutation();
+    assert_eq!(&data, &[1, 3, 2]);
+    data.prev_permutation();
+    assert_eq!(&data, &[1, 2, 3]);
+    assert!(!data.prev_permutation());
+    let mut c = 0;
+    while data.next_permutation() {
+        c += 1;
+    }
+    assert_eq!(c, 5);
+}
