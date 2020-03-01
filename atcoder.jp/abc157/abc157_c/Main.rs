9142b08e8ceda8f211a05d54d797601820fb6363
togatoga <togasakitogatoga@gmail.com>
2020-03-02 07:00:28 +0900

    [AC] abc157 abc157_c

---
+++ atcoder.jp/abc157/abc157_c/Main.rs
@@ -0,0 +1,118 @@
+fn solve() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let n: usize = sc.read();
+    let m: usize = sc.read();
+    let values: Vec<(usize, char)> = (0..m)
+        .map(|_| {
+            let s: usize = sc.read();
+            let c: i32 = sc.read();
+            (s - 1, std::char::from_digit(c as u32, 10).unwrap())
+        })
+        .collect();
+    let (start, end) = {
+        if n == 1 {
+            (0, 10)
+        } else if n == 2 {
+            (10, 100)
+        } else {
+            (100, 1000)
+        }
+    };
+    for x in start..end {
+        let x_str: Vec<char> = x.to_string().chars().collect();
+        let mut ok = true;
+        for &(s, c) in values.iter() {
+            if x_str[s] != c {
+                ok = false;
+            }
+        }
+        if ok {
+            println!("{}", x);
+            return;
+        }
+    }
+    println!("-1");
+}
+ 
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(64 * 1024 * 1024) // 64MB
+        .spawn(|| solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+pub mod utils {
+    const DYX: [(isize, isize); 8] = [
+        (0, 1),   //right
+        (1, 0),   //down
+        (0, -1),  //left
+        (-1, 0),  //top
+        (1, 1),   //down right
+        (-1, 1),  //top right
+        (1, -1),  //down left
+        (-1, -1), //top left
+    ];
+ 
+    pub fn try_adj(y: usize, x: usize, dir: usize, h: usize, w: usize) -> Option<(usize, usize)> {
+        let ny = y as isize + DYX[dir].0;
+        let nx = x as isize + DYX[dir].1;
+        if ny >= 0 && nx >= 0 {
+            let ny = ny as usize;
+            let nx = nx as usize;
+            if ny < h && nx < w {
+                Some((ny, nx))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+ 
+//snippet from kenkoooo
+pub struct Scanner<R> {
+    stdin: R,
+}
+ 
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_line(&mut self) -> String {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+ 
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
