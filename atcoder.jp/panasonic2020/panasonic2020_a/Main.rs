ea37fe17c3c95c7a79b5094e76ad91c855d9e2f7
togatoga <togasakitogatoga@gmail.com>
2020-03-14 21:02:29 +0900

    [AC] panasonic2020 panasonic2020_a

---
+++ atcoder.jp/panasonic2020/panasonic2020_a/Main.rs
@@ -0,0 +1,92 @@
+fn solve() {
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let x = vec![
+        1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4,
+        1, 51,
+    ];
+    let k: usize = sc.read();
+    println!("{}", x[k - 1]);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(64 * 1024 * 1024) // 64MB
+        .spawn(|| solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+pub mod utils {
+    const DYX: [(isize, isize); 8] = [
+        (0, 1),   //right
+        (1, 0),   //down
+        (0, -1),  //left
+        (-1, 0),  //top
+        (1, 1),   //down right
+        (-1, 1),  //top right
+        (1, -1),  //down left
+        (-1, -1), //top left
+    ];
+
+    pub fn try_adj(y: usize, x: usize, dir: usize, h: usize, w: usize) -> Option<(usize, usize)> {
+        let ny = y as isize + DYX[dir].0;
+        let nx = x as isize + DYX[dir].1;
+        if ny >= 0 && nx >= 0 {
+            let ny = ny as usize;
+            let nx = nx as usize;
+            if ny < h && nx < w {
+                Some((ny, nx))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
+//snippet from kenkoooo
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_line(&mut self) -> String {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
